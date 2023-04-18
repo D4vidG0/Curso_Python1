e28f1bc0b142261a6e4faa1109fb0fc1680b83d7
D4vidG0 <dagonzs@gmail.com>
2023-04-18 15:53:54 -0600

    Creacion de carpeta Tarea 2 y Ejercicio 1

--- Tarea 1/Tarea 2/Ejercicio 1.py
+++
@@ -1,3 +0,0 @@
-#Ejercicio 1 factorial numero
-num=int(input("Ingrese un numero mayor a 0: \n"))
-print(num)
---
+++ Tarea 2/Ejercicio1.py
@@ -0,0 +1,2 @@
+#Ejercicio 1 factorial de un numero ingresado por el usuario
+num=int(input("Ingrese un numero mayor a 0: \n"))
